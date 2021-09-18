bc7d17b5d4074a1c43269940670aa458a29dad81
Azamino28ki <miyamizu328@outlook.jp>
2021-09-18 16:02:48 +0900

    リファクタリング

--- scrape_spangbang.py
+++ scrape_spangbang.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
 
-def scrape_video_info(soup):
+def scrape_playlist(soup):
     video_infos = []
     for item in soup.find_all('div', attrs={'data-id':True}, class_='video-item', id=True):
 
@@ -39,36 +39,33 @@ def scrape_video_info(soup):
 
     return video_infos
 
+def download_mp4(file, url):
+    with open(file, 'wb') as saveFile:
+        # ファイルサイズ取得
+        if requests.head(url).status_code == 302 :
+            file_size = int(requests.head(url).headers["content-length"])
+            pbar = tqdm(total=file_size, unit="B", unit_scale=True)
+            # チャンクダウンロード
+            for chunk in requests.get(url, stream=True).iter_content(chunk_size=1024):
+                ff = saveFile.write(chunk)
+                pbar.update(len(chunk))
+            pbar.close()
+
 url = input("Search: ")
 
 r = requests.get(url).text
 soup = BeautifulSoup(r, 'lxml')
 
-print(scrape_video_info(soup))
-
 video_urls = []
 
-for video_info in scrape_video_info(soup):
+# 詳細画面
+for video_info in scrape_playlist(soup):
     r = requests.get(video_info['url']).text
     soup = BeautifulSoup(r, 'lxml')
-
+    # videoタグのsrcからvideoをダウンロード
     try:
         video_url = soup.video.source['src']
         if video_url is not None:
-            video_urls.append(video_url)
+            download_mp4(f'./{video_info["title"]}.mp4', video_url)
     except:
         print(f'No videos found for: {video_info["title"]}')
-
-for i, url in enumerate(video_urls, 1):
-    response = requests.get(url)
-    with open(f'./test_{i}.mp4', 'wb') as saveFile:
-        # ファイルサイズ取得
-        if requests.head(video_url).status_code == 302 :
-            video_url = requests.head(video_url).headers["Location"]
-        file_size = int(requests.head(video_url).headers["content-length"])
-        pbar = tqdm(total=file_size, unit="B", unit_scale=True)
-        # チャンクダウンロード
-        for chunk in requests.get(url, stream=True).iter_content(chunk_size=1024):
-            ff = saveFile.write(chunk)
-            pbar.update(len(chunk))
-        pbar.close()
