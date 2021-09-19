5d9e65ece1de905c8c45e439d46cd3805330e28b
Takuro Suetake <PassyonLips@outlook.jp>
2021-09-19 13:22:02 +0900

    リファクタリング

--- scrape_spangbang.py
+++ scrape_spangbang.py
@@ -8,32 +8,12 @@ def scrape_playlist(soup):
 
         try:
             full_video = item.find("a", class_='thumb')['href']
+            title = item.find("a", class_='n').string
         except:
-            print(f'No videos found for:')
-
-        # sometimes /category/ shows up
-        # which isn't a video link.
-        if "/category/" in full_video:
-            continue
-
-        # cba to find the proper title from html
-        # note that this may not always give proper title
-        title = full_video.split('/')[3].replace('+', ' ')
-
-        prev = item.picture.img
-        # sometimes preview image or preview video
-        # won't load for whatever reason.
-        try:
-            prev_vid = prev['data-preview']
-            image = prev['data-src']
-        except:
-            prev_vid = "Not Found"
-            image = "Not Found"
+            print(f'No videos found for:{item.attrs["data-id"]}')
         
         video_infos.append({
             'title': title,
-            'thumbnail': 'image',
-            'preview_video': 'prev_vid',
             'url': f'https://spankbang.com{full_video}',
         })
 
@@ -56,8 +36,6 @@ url = input("Search: ")
 r = requests.get(url).text
 soup = BeautifulSoup(r, 'lxml')
 
-video_urls = []
-
 # 詳細画面
 for video_info in scrape_playlist(soup):
     r = requests.get(video_info['url']).text
@@ -65,7 +43,8 @@ for video_info in scrape_playlist(soup):
     # videoタグのsrcからvideoをダウンロード
     try:
         video_url = soup.video.source['src']
-        if video_url is not None:
-            download_mp4(f'./{video_info["title"]}.mp4', video_url)
+        # print(video_info)
+        # if video_url is not None:
+        #     download_mp4(f'./{video_info["title"]}.mp4', video_url)
     except:
         print(f'No videos found for: {video_info["title"]}')
